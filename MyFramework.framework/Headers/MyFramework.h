ee1e814f4333223a1fcc98f9562115b9e10cc625
mauricio_mmarques <mauricio.mmarques1@gmail.com>
2020-04-26 17:55:53 -0400

    Create MyFramework project

---
+++ MyFramework.framework/Headers/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by Maurício Marques on 4/25/20.
+//  Copyright © 2020 MyOrganization. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
---
+++ MyFramework/MyFrameworkClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyFrameworkClass.swift
+//  MyFramework
+//
+//  Created by Maurício Marques on 4/25/20.
+//  Copyright © 2020 MyOrganization. All rights reserved.
+//
+
+import Foundation
+
+public class MyFrameworkClass {
+    
+    public init() { }
+    
+    public func logDescription() {
+        print("MyFramework class log")
+    }
+}
